6d217c620dd1949098a9239bec14d78a17633327
gokulg7 <07gokulg@gmail.com>
2024-12-05 14:38:33 +0530

    changed

--- src/main/java/com/carparking/project/entities/RoleStaging.java
+++ src/main/java/com/carparking/project/entities/RoleStaging.java
@@ -2,30 +2,37 @@ package com.carparking.project.entities;
 
 import com.carparking.project.domain.RoleDto;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "roles")
 public class RoleStaging {
 
     @Id
-    @Column(name="rolename")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     private String roleName;
 
     private String responsibilities;
 
     private String adminName;
 
-    public RoleStaging() {
-    }
+    public RoleStaging() {}
 
     public RoleStaging(RoleDto roleDto, String email) {
         this.roleName = roleDto.getRoleName();
         this.responsibilities = roleDto.getResponsibilities();
         this.adminName = email;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getRoleName() {
@@ -40,6 +47,10 @@ public class RoleStaging {
         return responsibilities;
     }
 
+    public void setResponsibilities(String responsibilities) {
+        this.responsibilities = responsibilities;
+    }
+
     public String getAdminName() {
         return adminName;
     }
@@ -47,8 +58,5 @@ public class RoleStaging {
     public void setAdminName(String adminName) {
         this.adminName = adminName;
     }
-
-    public void setResponsibilities(String responsibilities) {
-        this.responsibilities = responsibilities;
-    }
 }
+
--- src/main/java/com/carparking/project/repository/RoleStagingRepository.java
+++ src/main/java/com/carparking/project/repository/RoleStagingRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleStagingRepository extends CrudRepository<RoleStaging,String> {
+public interface RoleStagingRepository extends CrudRepository<RoleStaging, Integer> {
 }
