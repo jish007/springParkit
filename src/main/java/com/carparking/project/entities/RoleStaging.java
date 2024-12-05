eb60d06340d5e161c8b6080b7baff69890df10e7
gokulg7 <07gokulg@gmail.com>
2024-12-05 14:51:10 +0530

    changed

--- src/main/java/com/carparking/project/entities/RoleStaging.java
+++ src/main/java/com/carparking/project/entities/RoleStaging.java
@@ -2,37 +2,30 @@ package com.carparking.project.entities;
 
 import com.carparking.project.domain.RoleDto;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "roles")
 public class RoleStaging {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
+    @Column(name="rolename")
     private String roleName;
 
     private String responsibilities;
 
     private String adminName;
 
-    public RoleStaging() {}
+    public RoleStaging() {
+    }
 
     public RoleStaging(RoleDto roleDto, String email) {
         this.roleName = roleDto.getRoleName();
         this.responsibilities = roleDto.getResponsibilities();
         this.adminName = email;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getRoleName() {
@@ -47,10 +40,6 @@ public class RoleStaging {
         return responsibilities;
     }
 
-    public void setResponsibilities(String responsibilities) {
-        this.responsibilities = responsibilities;
-    }
-
     public String getAdminName() {
         return adminName;
     }
@@ -58,5 +47,8 @@ public class RoleStaging {
     public void setAdminName(String adminName) {
         this.adminName = adminName;
     }
-}
 
+    public void setResponsibilities(String responsibilities) {
+        this.responsibilities = responsibilities;
+    }
+}
