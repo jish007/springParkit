c99153af306db9e1d96b732e0da7f7e8cbf69b33
abhijith sreekumar <sreekumar.abhijith93@gmail.com>
2025-03-24 23:14:11 +0530

    [leave flow]
    [leave flow]

--- src/main/java/com/carparking/project/service/ProfileService.java
+++ src/main/java/com/carparking/project/service/ProfileService.java
@@ -44,9 +44,12 @@ public class ProfileService {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Autowired
+    SlotsService slotsService;
+
 
     //for app users
-    public String saveProfile(ProfileDto profileDto) throws Exception{
+    public String saveProfile(ProfileDto profileDto) throws Exception {
         Profile entity = new Profile();
         entity.setVehicleNumber(profileDto.getVehicleNumber());
         entity.setUserName(profileDto.getUserName());
@@ -70,16 +73,15 @@ public class ProfileService {
         entity.setFineAmount((double) 0);
         entity.setAdminMailId(loginRepository.getActiveUser());
         Profile profile = profileRepository.save(entity);
-        if( Objects.nonNull(profile)){
+        if (Objects.nonNull(profile)) {
             return "profile Is Created";
-        }
-        else{
+        } else {
             throw new Exception("profile Creation Failed");
         }
     }
 
     //for on-site users
-    public void saveOnSiteProfile(String vehicleNumber, Slots slots, String bookingSource) throws Exception{
+    public void saveOnSiteProfile(String vehicleNumber, Slots slots, String bookingSource) throws Exception {
         Profile profile = new Profile();
         profile.setVehicleNumber(vehicleNumber);
         profile.setAllocatedSlotNumber(slots.getSlotNumber());
@@ -98,10 +100,9 @@ public class ProfileService {
         profile.setBanned(false);
         profile.setFineAmount((double) 0);
         Profile obj = profileRepository.save(profile);
-        if (Objects.nonNull(obj)){
+        if (Objects.nonNull(obj)) {
             System.out.println("On site profile created");
-        }
-        else {
+        } else {
             throw new Exception("On-Site profile creation failed");
         }
     }
@@ -118,61 +119,60 @@ public class ProfileService {
         return profileRepository.findByVehicleNumber(vehicleNumber);
     }
 
-    public List<Profile> getProfiles(){
+    public List<Profile> getProfiles() {
         List<Profile> list = new ArrayList<>();
         profileRepository.findAll().iterator().forEachRemaining(list::add);
         return list;
     }
 
 
-
-    public  String updateProfile(String formId) throws Exception {
-        String jsonResponse =   jotFormSubmissions.getFormResponse(formId);
-        JsonResponse jsonResponse1 =objectMapper.readValue(jsonResponse, JsonResponse.class);
+    public String updateProfile(String formId) throws Exception {
+        String jsonResponse = jotFormSubmissions.getFormResponse(formId);
+        JsonResponse jsonResponse1 = objectMapper.readValue(jsonResponse, JsonResponse.class);
         List<FormContent> formdata = jsonResponse1.getContent();
-        Map<String, Answer> answers =   formdata.stream().map(form->form.getAnswers()).findFirst().get();
-        Answer vehicleData = answers.get("5");
+        Map<String, Answer> answers = formdata.stream().map(form -> form.getAnswers()).findFirst().get();
+        Answer vehicleData = answers.get("7");
         Answer email = answers.get("4");
         Answer fullName = answers.get("3");
 
         System.out.println(answers);
-        String vehiclenumber = vehicleData.getAnswer().toString().replaceAll("\\s","");
-        String emailData =email.getAnswer().toString().replaceAll("\\s","");
+        String vehiclenumber = vehicleData.getAnswer().toString().replaceAll("\\s", "");
+        String emailData = email.getAnswer().toString().replaceAll("\\s", "");
         String fullNamePrettyFormat = fullName.getPrettyFormat();
-        ;        String emailinfo = emailData.replace("\"", "");  // Remove all double quotes
+        ;
+        String emailinfo = emailData.replace("\"", "");  // Remove all double quotes
 
-       List<String> banned =  loginRepository.bannedUsers();
-       if(banned.contains(emailinfo)){
-           throw new Exception("user is banned");
-       }
+        List<String> banned = loginRepository.bannedUsers();
+        if (banned.contains(emailinfo)) {
+            throw new Exception("user is banned");
+        }
         String output = vehiclenumber.replace("\"", "");  // Remove all double quotes
         try {
             List<Profile> profiles = new ArrayList<>();
             profileRepository.findAll().forEach(profiles::add);
-            Profile profile =   profiles.stream().filter(p->p.getVehicleNumber().equals(output.toLowerCase())).collect(Collectors.toList()).get(0);
+            Profile profile = profiles.stream().filter(p -> p.getVehicleNumber().equals(output.toLowerCase())).collect(Collectors.toList()).get(0);
             System.out.println(profile);
             profile.setUserEmailId(emailinfo);
             profile.setUserName(fullNamePrettyFormat);
             profileRepository.save(profile);
-            UserDto userDto =new UserDto();
+            UserDto userDto = new UserDto();
             userDto.setEmail(emailinfo);
             userDto.setRoleName("USER");
-            userDto.setPassword(String.valueOf(Math.random())+"xxxx");
+            userDto.setPassword(String.valueOf(Math.random()) + "xxxx");
             userDto.setActive("");
-            loginService.signUp(userDto,"USER");
-        }
-        catch (Exception e){
-            System.out.println("e==="+e.getMessage());
+            loginService.signUp(userDto, "USER");
+        } catch (Exception e) {
+            System.out.println("e===" + e.getMessage());
 
         }
-        return  null;
+        return null;
     }
 
-    public Map<String,String> getAllTimer(){
+    public Map<String, String> getAllTimer() {
         LocalDateTime currentDate = LocalDateTime.now();
-        List<Profile> profiles =   profileRepository.findAll().stream().filter(p->p.getAdminMailId().equals("gokulgnair777@gmail.com")).collect(Collectors.toList());
-    return    profiles.stream()
-                .collect(Collectors.toMap(Profile::getVehicleNumber, p->duration(currentDate,convertToLocalDateTime(LocalDateTime.parse(p.getEndtime(), DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm:ss")).toLocalTime(),currentDate.toLocalDate()))));
+        List<Profile> profiles = profileRepository.findAll().stream().filter(p -> p.getAdminMailId().equals("gokulgnair777@gmail.com")).collect(Collectors.toList());
+        return profiles.stream()
+                .collect(Collectors.toMap(Profile::getVehicleNumber, p -> duration(currentDate, convertToLocalDateTime(LocalDateTime.parse(p.getEndtime(), DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm:ss")).toLocalTime(), currentDate.toLocalDate()))));
 
     }
 
@@ -181,7 +181,7 @@ public class ProfileService {
     }
 
 
-    public String duration(LocalDateTime currentDateTime, LocalDateTime endDateTime){
+    public String duration(LocalDateTime currentDateTime, LocalDateTime endDateTime) {
         Duration remainingDuration = Duration.between(currentDateTime, endDateTime);
         long hours = remainingDuration.toHours();
         long minutes = remainingDuration.toMinutes() % 60;
@@ -192,20 +192,20 @@ public class ProfileService {
 
 
     public String ban(String vehicleNumber) {
-        Profile profile =   profileRepository.findByVehicleNumber(vehicleNumber);
-        User user= loginRepository.findByEmail(profile.getUserEmailId());
+        Profile profile = profileRepository.findByVehicleNumber(vehicleNumber);
+        User user = loginRepository.findByEmail(profile.getUserEmailId());
         profile.setBanned(true);
         user.setIs_banned(true);
-        profile=profileRepository.save(profile);
-        if(Objects.nonNull(profile)){
+        profile = profileRepository.save(profile);
+        if (Objects.nonNull(profile)) {
             return "banned";
         }
         return "failed";
     }
 
 
-    public String rechargeProfile(String vehiclenumber, String duration){
-        Profile profile =     profileRepository.findByVehicleNumber(vehiclenumber);
+    public String rechargeProfile(String vehiclenumber, String duration) {
+        Profile profile = profileRepository.findByVehicleNumber(vehiclenumber);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime dateTime = LocalDateTime.parse(profile.getEndtime(), formatter);
         LocalDateTime newDateTime = addCustomDuration(dateTime, duration);
@@ -213,6 +213,8 @@ public class ProfileService {
         profileRepository.save(profile);
         return newDateTime.toString();
     }
+
+
 
 
 
@@ -226,11 +228,29 @@ public class ProfileService {
             String unit = matcher.group(2);
 
             switch (unit) {
-                case "h": dateTime = dateTime.plus(value, ChronoUnit.HOURS); break;
-                case "m": dateTime = dateTime.plus(value, ChronoUnit.MINUTES); break;
-                case "s": dateTime = dateTime.plus(value, ChronoUnit.SECONDS); break;
+                case "h":
+                    dateTime = dateTime.plus(value, ChronoUnit.HOURS);
+                    break;
+                case "m":
+                    dateTime = dateTime.plus(value, ChronoUnit.MINUTES);
+                    break;
+                case "s":
+                    dateTime = dateTime.plus(value, ChronoUnit.SECONDS);
+                    break;
             }
         }
         return dateTime;
+
+
+
+
     }
+
+    public void leaveSlotFlow(Profile profile, String slotnumber){
+        slotsService.leaveSlot(slotnumber);
+        profile.setAllocatedSlotNumber("");
+        profile.setEndtime(String.valueOf(0));
+    }
+
 }
+
